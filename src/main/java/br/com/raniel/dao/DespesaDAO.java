130b94b4ca0ff737a0824d0d97203876b1fc409c
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-04-13 12:28:54 -0300

    feat: implement interface

--- src/main/java/br/com/raniel/dao/DespesaDAO.java
+++ src/main/java/br/com/raniel/dao/DespesaDAO.java
@@ -1,4 +1,39 @@
 package br.com.raniel.dao;
 
-public class DespesaDAO {
+import br.com.raniel.model.Categoria;
+import br.com.raniel.model.Despesa;
+
+import java.util.List;
+import java.util.Optional;
+
+public class DespesaDAO implements IDespesaDAO {
+    @Override
+    public Despesa save(Despesa despesa) {
+        return null;
+    }
+
+    @Override
+    public Despesa update(Despesa despesa) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+
+    @Override
+    public List<Despesa> findAll() {
+        return null;
+    }
+
+    @Override
+    public Optional<Despesa> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Despesa> findByCategoria(Categoria categoria) {
+        return null;
+    }
 }
